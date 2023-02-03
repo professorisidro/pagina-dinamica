7f4af858414ba03a044a073bedfd5a222cdbe9df
professorisidro <isidro@professorisidro.com.br>
2023-02-02 21:52:01 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function mostraBio(id){
+    document.getElementById(id).style = "display: block;";
+}
+
+// function mostraBio1() {
+//     document.getElementById("bio1").style = "display:block;";
+// }
+
+// function mostraBio2() {
+//     document.getElementById("bio2").style = "display:block;";
+// }
+
+// function mostraBio3() {
+//     document.getElementById("bio3").style = "display:block;";
+// }
